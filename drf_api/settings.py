738289c89db388b5e14cc42cbddc1f25d889019d
Matt Manning <mtmanning93@hotmail.co.uk>
2023-09-15 10:24:41 +0000

    Bug fix in projects/views

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -63,7 +63,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'drf-apii-eb74b6f7eea1.herokuapp.com']
+ALLOWED_HOSTS = ['8000-mtmanning93-drfapi-iox54r178n7.ws-eu104.gitpod.io', 'localhost', 'drf-apii-eb74b6f7eea1.herokuapp.com']
 
 
 # Application definition
