80c969ff09e6c9f79203d714818e63f25f36afa5
Matt Manning <mtmanning93@hotmail.co.uk>
2023-09-15 10:14:16 +0000

    Updated deploy commit

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,8 +61,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# 'DEV' in os.environ
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['localhost', 'drf-apii-eb74b6f7eea1.herokuapp.com']
 
