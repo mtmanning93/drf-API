65fdc4f3372640bc1ef7ee60327313c9498e476c
Matt Manning <mtmanning93@hotmail.co.uk>
2023-09-15 10:42:31 +0000

    Update settings.py for deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import re
 import os
 import dj_database_url
 
@@ -63,7 +64,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-mtmanning93-drfapi-iox54r178n7.ws-eu104.gitpod.io', 'localhost', 'drf-apii-eb74b6f7eea1.herokuapp.com']
+ALLOWED_HOSTS = [
+    '8000-mtmanning93-drfapi-iox54r178n7.ws-eu104.gitpod.io',
+    'localhost',
+    os.environ.get('ALLOWED_HOST'),
+    ]
 
 
 # Application definition
@@ -106,13 +111,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(
+        r'^.+-',
+        os.environ.get('CLIENT_ORIGIN_DEV', ''),
+        re.IGNORECASE
+    ).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
