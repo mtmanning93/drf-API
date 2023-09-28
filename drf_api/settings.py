49f664473b111767ae1b330f73119f09130a8992
Matt Manning <mtmanning93@hotmail.co.uk>
2023-09-28 11:36:51 +0000

    CORS Fix attempt

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,7 +62,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-mtmanning93-drfapi-iox54r178n7.ws-eu104.gitpod.io',
